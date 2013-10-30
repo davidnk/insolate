6f6d888edf9dca1107f0dca6989fbfa1a48ff630
David Karesh <davidnk@gmail.com>
2013-10-30 01:19:53 -0400

    changed repo name

--- multi.py
+++ multi.py
@@ -1,21 +1,21 @@
 #! /usr/bin/python
 import os, sys, getpass, pexpect
-from subprocess import check_output, call, PIPE
+from subprocess import Popen, call, PIPE
 
 def call_init(remote_changes=None):
-    if os.path.exists(".multi"):
+    if os.path.exists(".multirepo"):
         exit("Already initialized.  To finish and backup run: mulit done <remote backup>")
     sh = """
-        git --git-dir=.multi --work-tree=. init
-        echo '.multi' >> .gitignore
+        git --git-dir=.multirepo --work-tree=. init
+        echo '.multirepo' >> .gitignore
         echo 'multi.py' >> .gitignore
         echo '.gitignore' >> .gitignore
-        git --git-dir=.multi add .
-        git --git-dir=.multi reset -- .multi
-        git --git-dir=.multi commit -a -m "Original files"
-        git --git-dir=.multi tag -a ORIG -m "original files"
-        git --git-dir=.multi branch CHANGES
-        git --git-dir=.multi checkout CHANGES
+        git --git-dir=.multirepo add .
+        git --git-dir=.multirepo reset -- .multirepo
+        git --git-dir=.multirepo commit -a -m "Original files"
+        git --git-dir=.multirepo tag -a ORIG -m "original files"
+        git --git-dir=.multirepo branch CHANGES
+        git --git-dir=.multirepo checkout CHANGES
         """
     call(sh, shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
     if remote_changes:
@@ -26,32 +26,33 @@ def call_pull(remote_changes):
     call_to('CHANGES')
     sh = """
         rsync -Pravdtze ssh %s .
-        git --git-dir=.multi add .
-        git --git-dir=.multi reset -- .multi
-        git --git-dir=.multi commit -a -m "Pulled changes"
+        git --git-dir=.multirepo add .
+        git --git-dir=.multirepo reset -- .multirepo
+        git --git-dir=.multirepo commit -a -m "Pulled changes"
         """ % remote_changes
     call(sh, shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
     return "Pulled updates"
 
 def call_to(branch):
-    if not os.path.exists(".multi"):
+    if not os.path.exists(".multirepo"):
         exit("missing .git; to initial run: multi init <remote changes>")
     sh = """
-        git --git-dir=.multi add .
-        git --git-dir=.multi reset -- .multi
-        git --git-dir=.multi commit -a -m "update"
-        git --git-dir=.multi checkout %s
-        git --git-dir=.multi reset --hard
+        git --git-dir=.multirepo add .
+        git --git-dir=.multirepo reset -- .multirepo
+        git --git-dir=.multirepo commit -a -m "update"
+        git --git-dir=.multirepo checkout %s
+        git --git-dir=.multirepo reset --hard
         """ % branch
     call(sh, shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
     return "Switched to %s" % branch
 
 def call_done(remote_changes=None):
-    if not os.path.exists(".multi"):
-        exit("missing .multi; to initial run: multi init <remote changes>")
+    if not os.path.exists(".multirepo"):
+        exit("missing .multirepo; to initial run: multi init <remote changes>")
     call_to("CHANGES")
     if remote_changes:
-        changes = check_output("git --git-dir=.multi diff --name-only ORIG CHANGES", shell=True)
+        changes = Popen("git --git-dir=.multirepo diff --name-only ORIG CHANGES",
+                shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate()[0]
         changes = changes.strip().split('\n')
         pswd = getpass.getpass(remote_changes.split(':')[0] + "'s password:")
         for f in changes:
@@ -60,9 +61,9 @@ def call_done(remote_changes=None):
             child.expect('password:')
             child.sendline(pswd)
             child.expect(pexpect.EOF)
-            print f + " \t\ttransfered"
+            print(f + " \t\ttransfered")
     call_to("ORIG")
-    call("rm -rf .multi", shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    call("rm -rf .multirepo", shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
     return "DONE"
 
 def main(argv):
@@ -73,26 +74,27 @@ def main(argv):
              "multi done [remote_changes]\t\tcopies changes back to remote and ends")
     if argv[0] == 'init':
         if len(argv) <= 2:
-            print call_init(*argv[1:])
+            print(call_init(*argv[1:]))
         else:
             exit("multi init [remote_changes]")
     elif argv[0] == 'pull':
         if len(argv) == 2:
-            print call_pull(argv[1])
+            print(call_pull(argv[1]))
         else:
             exit("multi pull <remote_changes>")
     elif argv[0] == 'to':
         if len(argv) < 2:
-            head = check_output('cat .multi/HEAD', shell=True)
+            head = Popen('cat .multirepo/HEAD', shell=True,
+                    stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate()[0]
             if 'ref: refs/heads/' in head:
-                print head.strip('ref: refs/heads/').strip('\n')
+                print(head.strip('ref: refs/heads/').strip('\n'))
             else:
-                print 'ORIG'
+                print('ORIG')
             exit("multi to <ORIG or CHANGES>")
-        print call_to(argv[1])
+        print(call_to(argv[1]))
     elif argv[0] == 'done':
         if len(argv) <= 2:
-            print call_done(*argv[1:])
+            print(call_done(*argv[1:]))
         else:
             exit("multi done [remote_changes]")
     else:
