927a546f1a49f4f08acebd4e29a9e614aa697715
David Karesh <davidnk@gmail.com>
2013-10-30 15:55:11 -0400

    Added general project files

--- insulater/insulate_edits.py
+++ insulater/insulate_edits.py
@@ -1,10 +1,19 @@
 #! /usr/bin/python
-import os, sys, getpass, pexpect
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+import sys
+import getpass
+import pexpect
 from subprocess import Popen, call, PIPE
+
 
 def call_init(remote_changes=None):
     if os.path.exists(".multirepo"):
-        exit("Already initialized.  To finish and backup run: mulit done <remote backup>")
+        exit("Already initialized. " +
+             " To finish and backup run: mulit done <remote backup>")
     sh = """
         git --git-dir=.multirepo --work-tree=. init
         echo '.multirepo' >> .gitignore
@@ -22,6 +31,7 @@ def call_init(remote_changes=None):
         call_pull(remote_changes)
     return "Initialized branches ORIG, CHANGES"
 
+
 def call_pull(remote_changes):
     call_to('CHANGES')
     sh = """
@@ -32,6 +42,7 @@ def call_pull(remote_changes):
         """ % remote_changes
     call(sh, shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
     return "Pulled updates"
+
 
 def call_to(branch):
     if not os.path.exists(".multirepo"):
@@ -46,17 +57,20 @@ def call_to(branch):
     call(sh, shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
     return "Switched to %s" % branch
 
+
 def call_done(remote_changes=None):
     if not os.path.exists(".multirepo"):
         exit("missing .multirepo; to initial run: multi init <remote changes>")
     call_to("CHANGES")
     if remote_changes:
-        changes = Popen("git --git-dir=.multirepo diff --name-only ORIG CHANGES",
-                shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate()[0]
+        changes = Popen(
+            "git --git-dir=.multirepo diff --name-only ORIG CHANGES",
+            shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE
+            ).communicate()[0]
         changes = changes.strip().split('\n')
         pswd = getpass.getpass(remote_changes.split(':')[0] + "'s password:")
         for f in changes:
-            rsync="rsync -Pravdtze ssh %s %s" % (f, remote_changes+f)
+            rsync = "rsync -Pravdtze ssh %s %s" % (f, remote_changes+f)
             child = pexpect.spawn(rsync)
             child.expect('password:')
             child.sendline(pswd)
@@ -66,12 +80,14 @@ def call_done(remote_changes=None):
     call("rm -rf .multirepo", shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE)
     return "DONE"
 
+
 def main(argv):
     if len(argv) == 0 or argv[0] == '-h' or argv[0] == '--help':
-        exit("multi init <remote_changes>\t\tstarts\n" + \
-             "multi to <ORIG or CHANGES>\t\tnavigates to branch\n" + \
-             "multi pull <remote_changes>\t\tpulls remote changes branch\n" + \
-             "multi done [remote_changes]\t\tcopies changes back to remote and ends")
+        exit("multi init <remote_changes>\t\tstarts\n" +
+             "multi to <ORIG or CHANGES>\t\tnavigates to branch\n" +
+             "multi pull <remote_changes>\t\tpulls remote changes branch\n" +
+             "multi done [remote_changes]\t\tcopies changes back to remote " +
+             "and ends")
     if argv[0] == 'init':
         if len(argv) <= 2:
             print(call_init(*argv[1:]))
@@ -85,7 +101,7 @@ def main(argv):
     elif argv[0] == 'to':
         if len(argv) < 2:
             head = Popen('cat .multirepo/HEAD', shell=True,
-                    stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate()[0]
+                         stdin=PIPE, stdout=PIPE, stderr=PIPE).communicate()[0]
             if 'ref: refs/heads/' in head:
                 print(head.strip('ref: refs/heads/').strip('\n'))
             else:
@@ -99,6 +115,7 @@ def main(argv):
             exit("multi done [remote_changes]")
     else:
         exit("Not a multi command. See 'multi --help'.")
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='insulater',
+    version='1.0.0',
+    packages=['insulater'],
+    author='David Karesh',
+    author_email='davidnk@gmail.com',
+    url='',
+    license='LICENSE.txt',
+    description='Tool to easily switch between original and modified versions of a directory.')
