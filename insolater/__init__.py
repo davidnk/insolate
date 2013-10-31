f148364dab1f57796780cc41d63c6f50cafa934d
David Karesh <davidnk@gmail.com>
2013-10-31 05:43:31 -0400

    added argparse for timeout and repo

--- insolater/__init__.py
+++ insolater/__init__.py
@@ -8,11 +8,19 @@ __all__ = [
     'pwd']
 
 # Export the global Commandr object methods.
-import sys
+import argparse
 from insolater import Insolater
-_INSOLATER = Insolater()
 
-command = lambda: _INSOLATER.main(sys.argv[1:])
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--timeout", type=int, default=5,
+                    help="set timeout for file transfers")
+parser.add_argument("-r", "--repo", type=str, default='.insolater_repo',
+                    help="set repository to store CHANGES and ORIG")
+parser.add_argument('cmd', nargs='+', help='command')
+args = parser.parse_args()
+_INSOLATER = Insolater(repo=args.repo, timeout=args.timeout)
+
+command = lambda: _INSOLATER.main(args.cmd)
 init = _INSOLATER.start_session
 push = _INSOLATER.push_remote
 pull = _INSOLATER.pull_remote
